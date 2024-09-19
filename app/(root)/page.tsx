ac94c3a7d97667bfcc5f4cc39f218e39ba799f6c
vohn237 <devohn99@outlook.com>
2024-09-19 09:28:33 -0500

    Add new pages and components, and Implement page layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalancebBox from '@/components/TotalBalancebBox';
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: 'Devohn',
+    lastName: 'Hall',
+    email: 'devohn99@outlook.com',
+  };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manager your account and transactions efficentely."
+          />
+          <TotalBalancebBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        Recent Transaction
+      </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500 }]}
+      />
+    </section>
+  );
 };
 
 export default Home;
