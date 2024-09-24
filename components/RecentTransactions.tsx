6cc23e50d562633848902595aade77a956ea23a6
vohn237 <devohn99@outlook.com>
2024-09-24 11:41:48 -0500

    added bank card function to MyBasnks

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -3,6 +3,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { BankTabItem } from './BankTabItem';
 import BankInfo from './BankInfo';
 import TransactionsTable from './TransactionsTable';
+import { Pagination } from './Pagination';
 
 const RecentTransactions = ({
   accounts,
@@ -10,10 +11,21 @@ const RecentTransactions = ({
   appwriteItemId,
   page = 1,
 }: RecentTransactionsProps) => {
+  const rowsPerPage = 10;
+  const totalPages = Math.ceil(transactions.length / rowsPerPage);
+
+  const indexOfLastTransaction = page * rowsPerPage;
+  const indexOfFirstTransaction = indexOfLastTransaction - rowsPerPage;
+
+  const currentTransactions = transactions.slice(
+    indexOfFirstTransaction,
+    indexOfLastTransaction
+  );
+
   return (
     <section className="recent-transactions">
       <header className="flex items-center justify-between">
-        <h2 className="recent-transactions-label">Recent Transactions</h2>
+        <h2 className="recent-transactions-label">Recent transactions</h2>
         <Link
           href={`/transaction-history/?id=${appwriteItemId}`}
           className="view-all-btn"
@@ -21,6 +33,7 @@ const RecentTransactions = ({
           View all
         </Link>
       </header>
+
       <Tabs defaultValue={appwriteItemId} className="w-full">
         <TabsList className="recent-transactions-tablist">
           {accounts.map((account: Account) => (
@@ -33,6 +46,7 @@ const RecentTransactions = ({
             </TabsTrigger>
           ))}
         </TabsList>
+
         {accounts.map((account: Account) => (
           <TabsContent
             value={account.appwriteItemId}
@@ -44,7 +58,14 @@ const RecentTransactions = ({
               appwriteItemId={appwriteItemId}
               type="full"
             />
-            <TransactionsTable transactions={transactions} />
+
+            <TransactionsTable transactions={currentTransactions} />
+
+            {totalPages > 1 && (
+              <div className="my-4 w-full">
+                <Pagination totalPages={totalPages} page={page} />
+              </div>
+            )}
           </TabsContent>
         ))}
       </Tabs>
