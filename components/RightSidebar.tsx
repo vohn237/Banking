28a26357982de3bfa45ce201769961e2323a7225
vohn237 <devohn99@outlook.com>
2024-09-24 19:48:56 -0500

    Finalize the app

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import React from 'react';
 import BankCard from './BankCard';
+import { countTransactionCategories } from '@/lib/utils';
+import Category from './Category';
 
 const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
+  const categories: CategoryCount[] = countTransactionCategories(transactions);
+
   return (
-    <aside
-      className="right-sidebar
-  "
-    >
+    <aside className="right-sidebar">
       <section className="flex flex-col pb-8">
         <div className="profile-banner" />
         <div className="profile">
@@ -16,6 +18,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
               {user.firstName[0]}
             </span>
           </div>
+
           <div className="profile-details">
             <h1 className="profile-name">
               {user.firstName} {user.lastName}
@@ -26,17 +29,17 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
       </section>
 
       <section className="banks">
-        <div className="flex w-ful justify-between">
+        <div className="flex w-full justify-between">
           <h2 className="header-2">My Banks</h2>
           <Link href="/" className="flex gap-2">
             <Image src="/icons/plus.svg" width={20} height={20} alt="plus" />
-            <h2 className="text-14 font-semibold">Add Banks</h2>
+            <h2 className="text-14 font-semibold text-gray-600">Add Bank</h2>
           </Link>
         </div>
 
         {banks?.length > 0 && (
-          <div className="relative flex flex-1 flex-col items-center justify-between gap-5">
-            <div className="relatice z-10">
+          <div className="relative flex flex-1 flex-col items-center justify-center gap-5">
+            <div className="relative z-10">
               <BankCard
                 key={banks[0].$id}
                 account={banks[0]}
@@ -56,6 +59,16 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
             )}
           </div>
         )}
+
+        <div className="mt-10 flex flex-1 flex-col gap-6">
+          <h2 className="header-2">Top categories</h2>
+
+          <div className="space-y-5">
+            {categories.map((category, index) => (
+              <Category key={category.name} category={category} />
+            ))}
+          </div>
+        </div>
       </section>
     </aside>
   );
